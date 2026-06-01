d48d5c0a0234c179925709314b9f0d3af06aa733
SinghaAnirban005 <anirbansingha20@gmail.com>
2026-06-02 01:11:06 +0530

    chore: add smtp server & email services

---
+++ server/src/services/email.ts
@@ -0,0 +1,28 @@
+import { redis } from "../redis"
+import { StoredEmail } from "../types"
+
+export class emailService {
+    private KEY: string
+
+    constructor(key: string) {
+        this.KEY = key
+    }
+
+    async saveEmail(email: StoredEmail) {
+        await redis.lpush(this.KEY, JSON.stringify(email))
+
+        await redis.ltrim(this.KEY, 0, 999)
+    }
+
+    async getEmails() {
+        const emails = await redis.lrange("emails", 0, 100)
+
+        return emails
+    }
+
+    async delEmails() {
+        await redis.del("emails")
+    }
+}  
+
+export const emailClient = new emailService("emails")
---
+++ server/src/smtp/index.ts
@@ -0,0 +1,25 @@
+import { SMTPServer } from "smtp-server";
+import { simpleParser } from "mailparser"
+import { emailQueue } from "../queue/queue";
+
+export const smtpServer = new SMTPServer({
+    authOptional: true,
+
+    async onData(stream, session, callback) {
+        try {
+            const parsedData = simpleParser(stream)
+    
+            await emailQueue.add(
+                "capture-email",
+    
+                {
+                    parsedData
+                }
+            )
+    
+            callback()
+        } catch (error) {
+            callback(error as Error)
+        }
+    }   
+})
