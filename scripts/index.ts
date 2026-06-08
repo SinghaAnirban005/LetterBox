2e9f79af5e36a7c7897082ee180c71de1853b5d4
SinghaAnirban005 <anirbansingha20@gmail.com>
2026-06-08 23:06:38 +0530

    feat: add scripts

---
+++ scripts/index.ts
@@ -0,0 +1,23 @@
+import nodemailer from "nodemailer";
+
+async function main() {
+  const transporter = nodemailer.createTransport({
+    host: "localhost",
+    port: 1025,
+    secure: false,
+  });
+
+  await transporter.sendMail({
+    from: "test@smtp-drain.dev",
+    to: "anirban@example.com",
+    subject: "SMTP Drain Test",
+    html: `
+      <h1>Hello!</h1>
+      <p>This email should appear in SMTP Drain.</p>
+    `,
+  });
+
+  console.log("Email sent");
+}
+
+main().catch(console.error);
